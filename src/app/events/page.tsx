a8f8b6728b00628cb656c92c0a10fdbbbc32ca1d
Alex macbookAir <aleksandrmisnov@gmail.com>
2025-02-13 23:29:10 +0000

    added events page starter and single event page

---
+++ src/app/events/page.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import apolloClient from "@/lib/apollo-client";
+import { EventListDocument } from "@/gql/graphql";
+
+export default async function Page() {
+  const { data } = await apolloClient.query({
+    query: EventListDocument,
+    // variables: {
+    //   slug: "pta-school-disco",
+    // },
+  });
+  const events = data.events;
+  console.log(data.events);
+  // return <h1>Events will go here</h1>;
+
+  return (
+    <div>
+      <h1 className="text-4xl mb-6 font-bold text-gray-700">Our Team</h1>
+      <div className="grid grid-cols-2 gap-6">
+        {events.map(event => {
+          return (
+            <Link
+              className="group grid grid-cols-[140px_1fr] bg-white shadow rounded-lg overflow-hidden relative hover:bg-gradient-to-r from-white to-amber-50"
+              key={event.id}
+              href={`/events/${event.slug}`}
+            >
+              <div className="relative overflow-hidden">
+                <img
+                  className="transition duration-300 absolute inset-0 h-full w-full object-cover group-hover:scale-125 group-hover:rotate-12"
+                  src={event.featuredImage.url}
+                />
+              </div>
+
+              <div className="p-4">
+                <p className="text-xl text-gray-600 font-bold group-hover:text-gray-700">
+                  {event.title}
+                </p>
+                <p className="text-sm text-gray-500 leading-6">{event.description}</p>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
