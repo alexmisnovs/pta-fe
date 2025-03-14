a0af1dbbdd2fa9375d8778576c1ad3493ca32be8
Alex macbookAir <aleksandrmisnov@gmail.com>
2025-03-14 16:14:53 +0000

    working on revalidate with apollo client and next

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,3 +1,17 @@
+// lib/client.js
+// import { HttpLink, InMemoryCache, ApolloClient } from "@apollo/client";
+// import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
+
+// export const { getClient } = registerApolloClient(() => {
+//   return new ApolloClient({
+//     cache: new InMemoryCache(),
+//     link: new HttpLink({
+//       uri: process.env.NEXT_PUBLIC_STRAPI_GRAPHQL_URL,
+//     }),
+//     ssrMode: typeof window === "undefined",
+//   });
+// });
+
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
