0f51e129d6bf214994736551cd4b372af3457e4b
Alex macbookAir <aleksandrmisnov@gmail.com>
2025-03-16 16:46:11 +0000

    fixed news bug not displaying if no image was provided

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -15,7 +15,7 @@ export default async function Page() {
     // },
   });
   const articles = data.articles;
-  // console.log(data.articles);
+  console.log(data.articles);
   // return <h1>articles will go here</h1>;
 
   return (
@@ -24,7 +24,7 @@ export default async function Page() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
         {articles.map(article => {
           if (!article) return null;
-          if (!article?.cover) return null;
+          // if (!article?.cover?.url) return null;
           return (
             <Link
               className="group grid grid-cols-[140px_1fr] bg-white shadow rounded-lg overflow-hidden relative hover:bg-gradient-to-r from-white to-amber-50"
@@ -32,10 +32,12 @@ export default async function Page() {
               href={`/news/${article.slug}`}
             >
               <div className="relative overflow-hidden">
-                <img
-                  className="transition duration-300 absolute inset-0 h-full w-full object-cover group-hover:scale-125 group-hover:rotate-12"
-                  src={article.cover.url}
-                />
+                {article.cover?.url && (
+                  <img
+                    className="transition duration-300 absolute inset-0 h-full w-full object-cover group-hover:scale-125 group-hover:rotate-12"
+                    src={article.cover.url}
+                  />
+                )}
               </div>
 
               <div className="p-4">
