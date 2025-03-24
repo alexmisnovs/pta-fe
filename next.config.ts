cea4d03c97e72e128f9fb4f44a3900670596ac95
Alex macbookAir <aleksandrmisnov@gmail.com>
2025-03-24 14:42:55 +0000

    netlify stuff

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: "export",
-  distDir: "build",
+  // distDir: "build",
   images: {
     unoptimized: true,
     remotePatterns: [
