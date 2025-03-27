4db8e4b35009990b3a851310878676d08cc35159
Alex macbookAir <aleksandrmisnov@gmail.com>
2025-03-27 22:17:51 +0000

    updated sort on query

--- src/components/HomePage/Articles.tsx
+++ src/components/HomePage/Articles.tsx
@@ -13,6 +13,7 @@ export default async function Articles() {
     },
     variables: {
       sort: "publishedAt:desc",
+      // sort: "updatedAt:desc",
       pagination: {
         limit: 3,
       },
